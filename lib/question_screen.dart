1ed0a0540914b14e6c55755dee6b18d63b6729eb
J00shs <j.surujpaul22@gmail.com>
2025-02-27 12:20:08 -0500

    Added google fonts, question/answer support

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,20 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class QuestionsScreen extends StatefulWidget{
+
+class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
+
   @override
   State<QuestionsScreen> createState() {
     return _QuestionsScreenState();
   }
-
 }
 
-//All widgets(stateful and stateless) need to have the build.
-//For stateful widgets, it will be in the states method. 
-class _QuestionsScreenState extends State<QuestionsScreen>{
-  @override
-  Widget build(BuildContext context) {
-   return Text("Quizzes");
+class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex =0;
+  void answeQuestion(){
+    setState((){
+      currentQuestionIndex++;
+    });
   }
-
+  @override
+  Widget build(context) {
+    final currentQuestion = questions[currentQuestionIndex];
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: GoogleFonts.lato(
+                fontSize: 24,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.getShuffledAnswers().map((item){
+              return AnswerButton(answerText: item, onTap: answeQuestion,);
+            }),
+          ],
+        ),
+      ),
+    );
+  }
 }
