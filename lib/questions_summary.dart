74f903eb3fac97d4460ba82d72b87fba6e91275c
J00shs <j.surujpaul22@gmail.com>
2025-03-17 18:28:59 -0400

    scrollable answers, text alignment, color changes

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -5,54 +5,60 @@ class QuestionsSummary extends StatelessWidget{
   
   final List<Map<String,Object>> summaryData;
 
-  // TODO: Design widget for summary page
   @override
   Widget build(context){
-    return Column(
-      children: summaryData.map(
-        (data){
-          return Row(
-            children:[
-              CircleAvatar(
-                backgroundColor: data['user_answer']== data['correct_answer'] ? Color.fromARGB(500,120, 189, 248):Color.fromARGB(450,228, 120, 247),
-                child: Text(
-                  ((data['question_index'] as int) +1).toString(),
-                  style: TextStyle(
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-              Column(
+    return SizedBox(
+      height: 350,
+      child: SingleChildScrollView(
+        child: Column(
+          children: summaryData.map(
+            (data){
+              return Row(
                 children:[
-                  Text(
-                    data['question'] as String,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      
+                  CircleAvatar(
+                    backgroundColor: data['user_answer']== data['correct_answer'] ? Color.fromARGB(500,120, 189, 248):Color.fromARGB(450,228, 120, 247),
+                    child: Text(
+                      ((data['question_index'] as int) +1).toString(),
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
                     ),
-                    ),
-                  const SizedBox(height:5,),
-                  Text(
-                    data['user_answer'] as String,
-                    style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.grey,
-                    ),
-                    ),
-                  Text(
-                    data['correct_answer'] as String,
-                    style: TextStyle(
-                      fontSize:  15,
-                      color: Color.fromARGB(500,120, 189, 248),
-                    )
                   ),
+                  const SizedBox(width: 10,),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children:[
+                      Text(
+                        data['question'] as String,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
+                        textAlign: TextAlign.left,
+                        ),
+                      const SizedBox(height:5,),
+                      Text(
+                        data['user_answer'] as String,
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.grey,
+                        ),
+                        ),
+                      Text(
+                        data['correct_answer'] as String,
+                        style: TextStyle(
+                          fontSize:  15,
+                          color: Color.fromARGB(500,120, 189, 248),
+                        )
+                      ),
+                    ],
+                  )
                 ],
-              )
-            ],
-          );
-        }
-      ).toList(),
+              );
+            }
+          ).toList(),
+        ),
+      ),
     );
   }
 }
